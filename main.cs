512e4d9a84f188fbddef0997f5b2f9470d35dad6
Rodrigo Javier Quinteros <64508573+rodrigo745@users.noreply.github.com>
2023-06-07 20:52:20 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -2,10 +2,10 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine "Bienvenido a Red Majestic
-    Operar con Visa - Ingrese 1
+    Console.WriteLine "Bienvenido a Red Majestic"
+    + "/nOperar con Visa - Ingrese 1"
     Operar con Mastercard - Ingrese 2
-    Operar con Diners Club - Ingrese 3
+    Operar con Dinerjzjss Club - Ingrese 3
     Ingrese su opción:";
   }
 }
