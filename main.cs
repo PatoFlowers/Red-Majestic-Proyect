9404dfd6a8c238c55927446feb6d503b77ab0e21
Rodrigo Javier Quinteros <64508573+rodrigo745@users.noreply.github.com>
2023-06-09 10:29:39 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,12 +1,55 @@
 using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //variables
+        int opcionTarjeta;
+        string[] nombreTarjetas = { "Visa", "Mastercard", "Diners Club" };
+        string identificador;
+        string digitosTarjeta = "";
+        bool validar = true;
 
-class Program {
-  public static void Main (string[] args) {
+        //Mensaje de inicio
+        Console.WriteLine("Bienvenido a Red Majestic" +
+                          "\nOperar con Visa - Ingrese 1" +
+                          "\nOperar con Mastercard - Ingrese 2" +
+                          "\nOperar con Diners Club -Ingrese 3" +
+                          "\nIngrese su opción:");
 
-    Console.WriteLine "Bienvenido a Red Majestic"
-    + "/nOperar con Visa - Ingrese 1"
-    Operar con Mastercard - Ingrese 2
-    Operar con Dinerjzjss Club - Ingrese 3
-    Ingrese su opción:";
-  }
+        opcionTarjeta = Int32.Parse(Console.ReadLine());
+
+
+        while (validar) // Controlo que el numero ingresado sea igual a 16
+        {
+            Console.Clear();
+            Console.WriteLine("RED MAJESTIC" +
+                              "\nTarjeta elegida: " + nombreTarjetas[opcionTarjeta - 1] +
+                              "\nIngrese los 16 digitos de la tarjeta: ");
+            digitosTarjeta = Console.ReadLine();
+            if (digitosTarjeta.Length == 16)
+            {
+               validar = false;
+            }
+        }
+        Console.WriteLine(digitosTarjeta);
+
+        // separo los 4 primeros numeros de la tarjeta
+        identificador = digitosTarjeta.Substring(0, 4);
+        
+
+        // siguiente: 
+
+
+
+        //funcion para validar la tarjeta
+        static string ValidarTarjeta(int tarjeta, int id)
+        {
+
+
+
+
+            return "as";
+        }
+    }
 }
